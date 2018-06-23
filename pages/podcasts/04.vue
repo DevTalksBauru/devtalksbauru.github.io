90c5648496d7cd7b8d02013d43c55f5d038bc008
“matheusagcosta” <matheusagcosta@gmail.com>
2018-06-23 09:43:12 -0300

    add new podcast about internship

---
+++ pages/podcasts/04.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  dt-podcast
+    h2(slot="title") Estágio
+    h4(slot="date") 23/06/2018
+    p(slot="description") Discussão sobre ser estagiário.
+    iframe(slot="audio", src="https://archive.org/embed/DT4EstagioEditando", width="100%", height="30")
+</template>
+
+<script>
+import DtPodcast from '~/components/Podcast'
+
+export default {
+  name: 'podcast-item',
+
+  components: { DtPodcast }
+}
+</script>
