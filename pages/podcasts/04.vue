4eb46eaab3703d02dec56e8d331b795fcd2db9fd
Gabriel Scavassa <gabriel.scavassa@outlook.com>
2018-06-23 15:19:52 +0100

    Updating episode description.

--- pages/podcasts/04.vue
+++ pages/podcasts/04.vue
@@ -2,7 +2,7 @@
   dt-podcast
     h2(slot="title") Estágio
     h4(slot="date") 23/06/2018
-    p(slot="description") Discussão sobre ser estagiário.
+    p(slot="description") Neste episódio, vamos discutir um pouo sobre ser estagiário. Falando de nossas experiências, o papel do mentor, vagas e dicas!
     iframe(slot="audio", src="https://archive.org/embed/DT4EstagioEditando", width="100%", height="30")
 </template>
 
