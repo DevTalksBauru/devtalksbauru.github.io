f43e93bf7ad1d26126fe23ac3e422578350844a3
gabriescavassa <gabriel.scavassa@eqs-global.com>
2018-06-25 13:44:47 +0100

    Update: Fixing podcast 04 description

--- pages/podcasts/04.vue
+++ pages/podcasts/04.vue
@@ -2,7 +2,7 @@
   dt-podcast
     h2(slot="title") Estágio
     h4(slot="date") 23/06/2018
-    p(slot="description") Neste episódio, discutimos sobre este momento mágico da vida. Falando das nossas experiências, o papel do mentor na formação, vagas incríveis que aparecem no Linkedin e de quebra algumas dicas!
+    p(slot="description") Neste episódio, discutimos sobre este momento mágico da vida. Falando das nossas experiências, o papel do mentor na formação, vagas incríveis que aparecem no Linkedin e de quebra, algumas dicas!
     iframe(slot="audio", src="https://archive.org/embed/DT4EstagioEditando", width="100%", height="30")
 </template>
 
